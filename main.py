3b46979f12a7d05f2e911e2c4f6bd6abd9cc5699
1chigo <100275808+StrawberriesforIchigo@users.noreply.github.com>
2022-03-16 14:03:56 +0000

    fixed the problems with the else statements changed the code for yourPersonality.

--- main.py
+++ main.py
@@ -7,78 +7,69 @@ Answer these questions to get your MBTI!
 
 yourPersonality = ''
 
-running = True
-
-while running == True:
-  print('''
+print('''
 Question1
 Do you find yourself drawing away from others or getting close to others?
-Answer \'I\' for drawing away from others and \'E\' for getting closer to others.
-        ''')
+  Answer \'I\' for drawing away from others and \'E\' for getting closer to others.
+          ''')
+q1 = input().upper()
+
+if q1 == 'I':
+  print('Oh! You are an introvert!')
+      
+elif q1 == 'E':
+  print('Wow! You\'re an extrovert!')
+      
+else :
+  print('Enter either I or E please.')
   q1 = input().upper()
-  if q1 == 'I':
-    yourPersonality += q1
-    print('Oh! You are an introvert!')
-  elif q1 == 'E':
-    yourPersonality += q1
-    print('Wow! You\'re an extrovert!')
-  else:
-    print('Enter either I or E')
-    q1 = input().upper()
-    
-  print('''
+         
+print('''
 Question2
 Do you prefer to look at the big picture, with all the possibilities, innovations and creative ideas or would you prefer to go by facts and reasoning, basing yourself on realistic ideals?
 Answer \'N\' if you look towards the big picture, or \'S\' if you are a realistic person.
-        ''')
+          ''')
+q2 = input().upper()
+if q2 == 'N':
+  print('That must mean you are intuitive! Nice!')
+elif q2 == 'S':
+  print('You would be a Sensing kind of person. That\'s pretty cool!')
+else :
+  print('Enter either N or S please.')
   q2 = input().upper()
-  if q2 == 'N':
-    yourPersonality += q2
-    print('That must mean you are intuitive! Nice!')
-  elif q2 == 'S':
-    yourPersonality += q2
-    print('You would be a Sensing kind of person. That\'s pretty cool!')
-  else:
-    ('Enter either N or S please.')
-    q2 = input().upper()
-
-  print('''
+  
+print('''
 Question3
 Do you tend to make decisions based on logical analysis, based off of fairness, pros and cons, etc, or are you more likely to make decisions emotionally or with sentiment, based on your own personal values, thoughts and opinions?
 Answer \'T\' if you make decisions logically or \'F\' if you make decisions sentimentally.
-        ''')
+          ''')
+q3 = input().upper()
+if q3 == 'T':
+  print('Your\'e a Thinker. That\'s amazing!')
+elif q3 == 'F':
+  print('Being a Feel')
+else :
+  print('Enter either T or F please.')
   q3 = input().upper()
-  if q3 == 'T':
-    yourPersonality += q3
-    print('Your\'e a Thinker. That\'s amazing!')
-  elif q3 == 'F':
-    yourPersonality += q3
-    print('Being a Feel')
-  else:
-    ('Enter either T or F please.')
-     q3 = input().upper()
-
-  print('''
+  
+print('''
 Question4
-Okay final question here! Do you feel more comfortable abiding to the rules, 
-sticking to plans, and tend to be more organized or do you prefer to have your 
-options open, act spontaneously, and be flexible with your plans?
-Answer \'J\' if you are the rule abider or \'P\' if you like to be flexible with 
-your options.
-       ''')
+Okay final question here! Do you feel more comfortable abiding to the rules, sticking to plans, and tend to be more organized or do you prefer to have your options open, act spontaneously, and be flexible with your plans?
+Answer \'J\' if you are the rule abider or \'P\' if you like to be flexible with your options.
+         ''')
+q4 = input().upper()
+if q4 == 'J':
+  print('So you\'re a judger! Super!')
+elif q4 == 'P':
+  print('Perceivers are sooo wonderful though.')
+else :
+  print('Enter either J or P please.')
   q4 = input().upper()
-  if q4 == 'J':
-    yourPersonality += q4
-    print('So you\'re a judger! Super!')
-  elif q4 == 'P':
-    yourPersonality += q4
-    print('Perceivers are sooo wonderful though.')
-  else:
-    ('Enter either J or P please.')
-    q4 = input().upper()
+
+yourPersonality = q1 + q2 + q3 + q4
     
-  print ('Oh! you have completed the test your personality type is ' + yourPersonality + '!')
-  break
+print ('Oh! you have completed the test your personality type is ' + yourPersonality + '!')
+ 
   
   
 
